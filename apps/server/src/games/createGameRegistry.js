645fa1779012ac7927dc7a578c278c791d700a7b
Joe King <35816798+jk1551@users.noreply.github.com>
2026-08-09 22:20:49 -0400

    feat: register Fakin' It game

--- apps/server/src/games/createGameRegistry.js
+++ apps/server/src/games/createGameRegistry.js
@@ -1,9 +1,15 @@
 import { GameRegistry } from "./GameRegistry.js";
 import { BluffPartyGame } from "./bluff-party/BluffPartyGame.js";
+import { FakinItGame } from "./fakin-it/FakinItGame.js";
 
 export function createGameRegistry() {
-  return new GameRegistry().register(
-    "bluff-party",
-    (context) => new BluffPartyGame(context)
-  );
+  return new GameRegistry()
+    .register(
+      "bluff-party",
+      (context) => new BluffPartyGame(context)
+    )
+    .register(
+      "fakin-it",
+      (context) => new FakinItGame(context)
+    );
 }
